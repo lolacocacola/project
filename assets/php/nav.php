a424fb445c5a4a00c79bbc88e5fd6fc79ddf46c3
lolacocacola <olesea.nistor@gmail.com>
2023-02-23 18:24:15 +0200

    add files of the project

---
+++ assets/php/nav.php
@@ -0,0 +1,46 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<div class="nav-container">
+    <div class="mobile-topbar-header">
+        <div class="">
+            <img src="assets/images/logo-icon.png" class="logo-icon-2" alt="" />
+        </div>
+        <div>
+            <h4 class="logo-text">Supedia</h4>
+        </div>
+        <a href="javascript:;" class="toggle-btn ml-auto"> <i class="bx bx-menu"></i>
+        </a>
+    </div>
+    <nav class="topbar-nav">
+        <ul class="metismenu" id="menu">
+            <li>
+                <a href="/index.php" class="has-arrow">
+                    <div class="parent-icon icon-color-1"><i class="bx bx-home-alt"></i>
+                    </div>
+                    <div class="menu-title">Home</div>
+                </a>
+            </li>
+            <li>
+                <a href="/assets/php/shop.php" class="has-arrow">
+                    <div class="parent-icon icon-color-2"><i class='bx bx-briefcase-alt'></i>
+                    </div>
+                    <div class="menu-title">Shop</div>
+                </a>
+            </li>
+            <li>
+                <a  href="assets/php/contact.php" class="has-arrow">
+                    <div class="parent-icon icon-color-1"><i class="bx bx-line-chart"></i>
+                    </div>
+                    <div class="menu-title">Contact Us</div>
+                </a>
+            </li>
+            <li>
+                <a  href="assets/php/about.php" class="has-arrow">
+                    <div class="parent-icon icon-color-2"> <i class="bx bx-donate-blood"></i>
+                    </div>
+                    <div class="menu-title">About</div>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
